cbb8db3bc6e88486cb76e7f8de2e990dbcf2db41
ikaverin <kaverin1990@gmail.com>
2021-12-10 03:43:46 +0300

    fix bugs

--- src/index.js
+++ src/index.js
@@ -1,66 +1,101 @@
-import calculation, { ops, isValid, sqrt, percent } from './service.js';
+import calculation, {
+  ops, isValid, sqrt, percent,
+} from './service.js';
 
 let display = '';
 const lastChar = (str) => str[str.length - 1];
 
+const expression = document.querySelector('.grid');
+const screen = document.querySelector('.expression');
+const result = document.querySelector('.result');
 
-let expression = document.querySelector('.grid');
-let screen = document.querySelector('.expression');
-let result = document.querySelector('.result');
-
+function render() {
+    screen.innerText = display;
+  }
 
 render();
 
 expression.addEventListener('click', (e) => {
-    const point = Object.entries(e.target.dataset);
-    
-    for(const [key, value] of point) {
-        if(key === 'num' || key === 'braces') {
-            display += value ==='.' && display.length === 0 ? '0.' : value;
-            render()
-        }
-        if(key === 'zero' && display.length !== 0) {
-            display += value;
-            render()
-        }
-        if(value === 'clear') {
-            display = '';
-            result.innerText = ''
-            render()
-        }
-        if(value === 'sqrt' && !isNaN(lastChar(display))) {
-            const a = sqrt(display);
-            display = a;
-            render()
-        }
-        if(value === 'percent' && !isNaN(lastChar(display))) {
-            const b = percent(display);
-            display = b;
-            render();
-        }
-        if((key === 'operation' && !ops.includes(lastChar(display))) && (lastChar(display) !== '.' && display.length !== 0)) {
-            display += value;
-            render()
-            
-        }
-        if(value === '=' && !ops.includes(lastChar(display)) ) {
+  const point = Object.entries(e.target.dataset);
 
-            const output = display.length === 0 ? 'There is no input' : 
-                       isValid(display) === false ? 'incorrect input' :
-                       calculation(display)
-
-            result.innerHTML = output;
-        }
-        
-        
+  for (const [key, value] of point) {
+      result.innerText = ''
+    if (key === 'num' || key === 'braces') {
+      display += value === '.' && display.length === 0 ? '0.' : value;
+      render();
     }
-    
-    
+    if (key === 'zero' && display.length !== 0) {
+      display += value;
+      render();
+    }
+    if (value === 'clear') {
+      display = '';
+      result.innerText = '';
+      render();
+    }
+    if (value === 'sqrt' && !isNaN(lastChar(display))) {
+      const a = sqrt(display);
+      display = a;
+      render();
+    }
+    if (value === 'percent' && !isNaN(lastChar(display))) {
+      const b = percent(display);
+      display = b;
+      render();
+    }
+    if ((key === 'operation' && !ops.includes(lastChar(display))) && (lastChar(display) !== '.' && display.length !== 0)) {
+      display += value;
+      render();
+    }
+    if (value === '=' && !ops.includes(lastChar(display))) {
+      const output = display.length === 0 ? 'There is no input'
+        : isValid(display) === false ? 'incorrect input'
+          : calculation(display);
+
+      result.innerText = output;
+      display = output;
+      render();
+    }
+  }
+});
+
+document.addEventListener('keypress', (e) => {
+    const point = Object.entries(e.target.dataset);
+
+    const eventCodes = ['1','2','3','4','5','6','7','8','9','0', '/', '+', '-', '(', ')', 'x',];
+    const percentKey = 36;
+    const enter = 13;
+    const square = 8730;
+    const clear = 32;
+
+    if(eventCodes.includes(e.key)) {
+        display += e.key;
+        render()
+    }
+
+    if (e.key === 'Enter' && !ops.includes(lastChar(display))) {
+        const output = display.length === 0 ? 'There is no input'
+          : isValid(display) === false ? 'incorrect input'
+            : calculation(display);
+  
+        result.innerText = output;
+        display = output;
+        render();
+      }
+      if (e.key === ' ') {
+        display = '';
+        result.innerText = '';
+        render();
+      }
+      if (e.key === '√' && !isNaN(lastChar(display))) {
+        const a = sqrt(display);
+        display = a;
+        render();
+        result.innerText = a
+      }
+
+
+    console.log(e.key, 'dis',display)
+
+
 })
-
-function render()  {
-    screen.innerText = display;
-}
-
-
-
--- src/service.js
+++ src/service.js
@@ -1,121 +1,113 @@
 export const ops = ['+', '-', '/', 'x'];
 
 const priority = {
-    "x": 1,
-    "/": 1,
-    "+": 0,
-    "-": 0,
-  };
-  const operators = {
-    "+": (x, y) => x + y,
-    "-": (x, y) => x - y,
-    "x": (x, y) => x * y,
-    "/": (x, y) => x / y,
-  };
-  
-  export const isValid = (s) => {
-    
-    const open = ['(','{','['];
-    const close = [')','}',']'];
-    const stack = [];
-    
-    for(let i = 0; i < s.length; i += 1) {
-        if(open.includes(s[i])) {
-            stack.push(s[i])
-        }
-        if(close.includes(s[i])) {
-            if(close.indexOf(s[i]) !== open.indexOf(stack[stack.length - 1])) {
-                return false
-            } else {
-              stack.pop()
-            }
-        }
+  'x': 1,
+  '/': 1,
+  '+': 0,
+  '-': 0,
+  '*': 1,
+};
+const operators = {
+  '+': (x, y) => x + y,
+  '-': (x, y) => x - y,
+  'x': (x, y) => x * y,
+  '/': (x, y) => x / y,
+  '*': (x, y) => x * y,
+};
+
+export const isValid = (s) => {
+  const open = ['(', '{', '['];
+  const close = [')', '}', ']'];
+  const stack = [];
+
+  for (let i = 0; i < s.length; i += 1) {
+    if (open.includes(s[i])) {
+      stack.push(s[i]);
     }
-    return stack.length === 0
+    if (close.includes(s[i])) {
+      if (close.indexOf(s[i]) !== open.indexOf(stack[stack.length - 1])) {
+        return false;
+      }
+      stack.pop();
+    }
   }
+  return stack.length === 0;
+};
 
-  export const sqrt = (str) => {
-    const arr = str.split(/([\+\-\x\/\(\)])/);
-    return arr
-    .map((item, index) => 
-    index === arr.length - 1 ? parseFloat(Math.sqrt(item)).toFixed(2) : item)
-    .join('')
-}
+export const sqrt = (str) => {
+  const arr = str.split(/([\+\-\x\/\(\)])/); // 
+  return arr
+    .map((item, index) => (index === arr.length - 1 ? isInt(Math.sqrt(item)) : item))
+    .join('');
+};
 export const percent = (str) => {
-    const arr = str.split(/([\+\-\x\/\(\)])/) ///   
-    // if(arr.length === 1) {arr[0] = 100}
-    for(let i = arr.length - 2; i >= 0; i -= 1) {
-     
-        if(!isNaN(arr[i])) {
-            arr[arr.length - 1] = arr[i] * arr[arr.length - 1] / 100; 
-            break;
-        }
-
+  const arr = str.split(/([\+\-\x\/\(\)])/);
+  if(arr.length === 1) {
+      return arr[0] / 100
+  }
+  for (let i = arr.length - 2; i >= 0; i -= 1) {
+    if (!isNaN(arr[i])) {
+      arr[arr.length - 1] = (arr[i] * arr[arr.length - 1]) / 100;
+      break;
     }
-    return arr.join('')
-}
+  }
+  return arr.join('');
+};
 
 const isInt = (str) => {
-    const a = Number(str)
-    return Number.isInteger(a) ? str : parseFloat(str).toFixed(2)
-  }
-
-  
+  const a = Number(str);
+  return Number.isInteger(a) ? str : parseFloat(str).toFixed(2);
+};
 
 const infixIntoPolish = (str) => {
-    const arr = str.split(/([\+\-\x\/\(\)])/); // 
-    
-    // console.log(arr)
-    const opsStack = [];
-    const peek = (a) => a[a.length - 1];
-    
-    const result = arr.reduce((exitStack, sym) => {
-      if (parseFloat(sym)) {
-        exitStack.push(sym);
-      }
-      if (sym == "(") {
-        opsStack.push(sym);
-      }
-  
-      if (sym == ")") {
-        while (peek(opsStack) != "(") exitStack.push(opsStack.pop());
-        opsStack.pop();
-      }
-  
-      if (sym in priority) {
-        while (
-          peek(opsStack) in priority &&
-          priority[sym] <= priority[peek(opsStack)]
-        )
-          exitStack.push(opsStack.pop());
-        opsStack.push(sym);
-      }
-  
-      return exitStack;
-    }, []);
-    const reversed = opsStack.reverse();
-  
-    return [...result, ...reversed];
-  };
-  
-  
-  
-  const calculate = (array) => {
-    const stack = [];
-  
-    array.forEach((sym) => {
-      if (sym in operators) {
-        const [y, x] = [stack.pop(), stack.pop()];
-        
-        stack.push(operators[sym](x, y));
-      } else {
-        stack.push(parseFloat(sym));
-      }
-    });
-  
-    return  isInt(stack.pop())
-  };
+  const arr = str.split(/([\+\-\x\/\(\)])/); // regex to split string;
 
-  export default (str) => calculate(infixIntoPolish(str))
- 
- 
+  // console.log(arr)
+  const opsStack = [];
+  const peek = (a) => a[a.length - 1];
+
+  const result = arr.reduce((exitStack, sym) => {
+    if (parseFloat(sym)) {
+      exitStack.push(sym);
+    }
+    if (sym === '(') {
+      opsStack.push(sym);
+    }
+
+    if (sym === ')') {
+      while (peek(opsStack) !== '(') exitStack.push(opsStack.pop());
+      opsStack.pop();
+    }
+
+    if (sym in priority) {
+      while (
+        peek(opsStack) in priority
+          && priority[sym] <= priority[peek(opsStack)]
+      ) exitStack.push(opsStack.pop());
+      opsStack.push(sym);
+    }
+
+    return exitStack;
+  }, []);
+  const reversed = opsStack.reverse();
+
+  return [...result, ...reversed];
+};
+
+const calculate = (array) => {
+  const stack = [];
+
+  array.forEach((sym) => {
+    if (sym in operators) {
+      const [y, x] = [stack.pop(), stack.pop()];
+
+      stack.push(operators[sym](x, y));
+    } else {
+      stack.push(parseFloat(sym));
+    }
+  });
+
+  return isInt(stack.pop());
+};
+
+export default (str) => calculate(infixIntoPolish(str));
